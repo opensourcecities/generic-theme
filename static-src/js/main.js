5b90d9a7cb5a6285ff99aa2db076f37393759e45
Khosrow Moossavi <khos2ow@gmail.com>
2018-07-14 19:44:53 -0400

    Move 'source' (less, js) from static folder to its own (#14)

--- static-src/js/main.js
+++ static-src/js/main.js
@@ -3,17 +3,21 @@
  * Code licensed under the Apache License v2.0.
  * For details, see http://www.apache.org/licenses/LICENSE-2.0.
  */
-
 (function($) {
     "use strict"; // Start of use strict
 
     // jQuery for page scrolling feature - requires jQuery Easing plugin
     $('a.page-scroll').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: ($($anchor.attr('href')).offset().top - 50)
-        }, 1250, 'easeInOutExpo');
-        event.preventDefault();
+        if ( !window.event.ctrlKey && !window.event.shiftKey) {
+            var $anchor = $(this);
+            var $target = $('#' + $anchor.data('target'));
+            if ( $target.length > 0 ) {
+                event.preventDefault();
+                $('html, body').stop().animate({
+                    scrollTop: ($target.offset().top - 50)
+                }, 1250, 'easeInOutExpo');
+            }
+        }
     });
 
     // Highlight the top nav as scrolling occurs
@@ -45,4 +49,13 @@
     // Initialize WOW.js Scrolling Animations
     new WOW().init();
 
+    // Open modal for Thumbnail record items
+    $('a.record-link').bind('click', function(event) {
+        if ( !window.event.ctrlKey && !window.event.shiftKey) {
+            event.preventDefault();
+            event.stopPropagation();
+
+            $($(this).data('target')).modal();
+        }
+    });
 })(jQuery); // End of use strict
